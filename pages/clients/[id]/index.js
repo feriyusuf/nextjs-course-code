acd4f05ab4f5968be4b26aa298a4b47b8142f9c3
Feri Yusuf <feri.yusuf@bankraya.co.id>
2023-04-21 23:01:28 +0700

    66. Navigating Programmatically

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -3,12 +3,23 @@ import { useRouter } from 'next/router';
 function ClientProjectsPage() {
   const router = useRouter();
   const query = router.query;
-
   console.log(query);
+
+  const loadProjectHandler = () => {
+    // load data ...
+    router.push({
+      pathname: '/clients/[id]/[clientprojectid]',
+      query: {
+        id: query.id,
+        clientprojectid: 'next.js-course',
+      },
+    });
+  };
 
   return (
     <div>
       <h1>The Projects of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
