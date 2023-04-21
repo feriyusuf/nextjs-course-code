53bbd76d58decfbaea44058aa978e6b959197e9b
Feri Yusuf <feri.yusuf@bankraya.co.id>
2023-04-21 22:33:17 +0700

    64. Navigating To Dynamic Routes

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,10 +1,30 @@
+import Link from 'next/link';
+
 function ClientsPage() {
+  const clients = [
+    { id: 'feri', name: 'Feri' },
+    { id: 'yusuf', name: 'Yusuf' },
+    { id: 'nada', name: 'Nada' },
+    { id: 'nuri', name: 'Nuri' },
+    { id: 'awaliyah', name: 'Awaliyah' },
+    { id: 'vani', name: 'Vani' },
+    { id: 'nurbayani', name: 'Nurbayani' },
+  ];
+
   return (
     <div>
       <h1>The Clients Page</h1>
+      <ul>
+        {clients.map((client) => {
+          return (
+            <li key={client.id}>
+              <Link href={`/clients/${client.id}`}>{client.name}</Link>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
 
 export default ClientsPage;
- 
