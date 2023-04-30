84a7f77778a8dd2f3bac56c860eb9bda19a4f7f7
Feri Yusuf <feri.yusuf@bankraya.co.id>
2023-04-30 21:01:55 +0700

    112. Implementing Client-Side Data Fetching

---
+++ pages/last-sales.js
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react';
+
+function LastSalesPage(props) {
+  const [sales, setSales] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(
+      'https://nextjs-course-code-b9589-default-rtdb.asia-southeast1.firebasedatabase.app/sales.json'
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        const transformedSales = [];
+
+        for (const key in data) {
+          transformedSales.push({
+            id: key,
+            username: data[key].username,
+            volume: data[key].volume,
+          });
+        }
+
+        setSales(transformedSales);
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (!sales) {
+    return <p>No data yet</p>
+  }
+
+  return (
+    <ul>
+      {sales.map((sale) => {
+        return (
+          <li key={sale.id}>
+            {sale.username} 👉🏻 {sale.volume}
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+export default LastSalesPage;
