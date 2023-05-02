004e0b768241b86697cfa4a7ecbc02f917975fce
Feri Yusuf <feri.yusuf@bankraya.co.id>
2023-05-02 08:47:06 +0700

    DIMS: fetching data on homescreen

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,59 @@
-import { getFeaturedEvents } from '../dummy-data';
+import { useEffect, useState } from 'react';
 import EventList from '../components/events/event-list';
+import useSWR from 'swr';
 
-function HomePage() {
-  const featuredEvents = getFeaturedEvents();
+const fetcher = (url) =>
+  fetch(url)
+    .then((r) => r.json())
+    .then((data) => {
+      return Object.keys(data)
+        .map((key) => {
+          return { ...data[key], id: key };
+        })
+        .filter((event) => event.isFeatured);
+    });
+
+function HomePage(props) {
+  const [events, setEvents] = useState(props.events);
+  const { data, error } = useSWR(
+    'https://nextjs-course-code-b9589-default-rtdb.asia-southeast1.firebasedatabase.app/events.json',
+    fetcher
+  );
+
+  useEffect(() => {
+    if (data) {
+      setEvents(data);
+    }
+  }, [data]);
+
+  if (error) {
+    return <p>Failed to load.</p>;
+  }
+
+  if (!data && !events) {
+    return <p>Loading...</p>;
+  }
 
   return (
     <div>
-      <EventList items={featuredEvents} />
+      <EventList items={events} />
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const response = await fetch(
+    'https://nextjs-course-code-b9589-default-rtdb.asia-southeast1.firebasedatabase.app/events.json'
+  );
+
+  const data = await response.json();
+  const events = Object.keys(data)
+    .map((key) => {
+      return { ...data[key], id: key };
+    })
+    .filter((event) => event.isFeatured);
+
+  return { props: { events } };
 }
 
 export default HomePage;
